790a9f77b7723a102fc8b9a1b4982ab4ee4e7e51
dinaranggiaa <dinaranggiaa@gmail.com>
2020-10-05 09:59:28 +0700

    File index php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html langj="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan GIT Remote</title>
+
+</head>
+
+<body>
+    <h1>Hello World!!</h1>
+
+</body>
+
+</html>
